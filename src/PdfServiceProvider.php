2a0d74bc07668d41421105a571266932b2517905
Ashik Ahmed <md.ashikahmed@outlook.com>
2024-09-23 13:39:30 +0600

    provider is deferred

--- src/PdfServiceProvider.php
+++ src/PdfServiceProvider.php
@@ -7,17 +7,24 @@ use Illuminate\Support\ServiceProvider;
 class PdfServiceProvider extends ServiceProvider
 {
     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
      * Register services.
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(
             __DIR__ . '/../config/pdf.php', 'pdf'
         );
 
-        $this->app->singleton('pdf', function ($app) {
+        $this->app->bind('pdf', function ($app) {
             return new Pdf();
         });
     }
@@ -28,11 +35,21 @@ class PdfServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->publishes([
             __DIR__.'/../config/pdf.php' => config_path('pdf.php')
         ], 'pdf-config');
 
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return array('pdf');
     }
 }
